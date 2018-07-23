28453085504783ac12233996d414752d3d03276d
lizardskins <riley.barlow@gmail.com>
2018-07-22 20:15:07 -0600

    Herkou fix

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./FriendFinder/app/routing/apiRoutes")(app);
+require("./FriendFinder/app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT)
